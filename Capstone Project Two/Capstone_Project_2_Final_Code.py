e03c44633a8efb322c76156b9022402eecc9bbe3
Asif Bala <asifbala@Asifs-MacBook.gardner-webb.net>
2019-05-04 12:53:57 -0400

    update

--- Capstone Project Two/Capstone_Project_2_Final_Code.py
+++ Capstone Project Two/Capstone_Project_2_Final_Code.py
@@ -9,12 +9,21 @@ from keras.applications.imagenet_utils import preprocess_input
 from keras.layers import Dense, Activation, Flatten, Conv2D
 from keras.layers import MaxPooling2D, Dropout
 from keras.models import Sequential
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing import image
+from keras.models import Model
 
 warnings.simplefilter("ignore", category=DeprecationWarning)
 
 train_labels = '/Users/asifbala/Desktop/humpback-whale-identification.zip Folder/train.csv'
 
 train_df = pd.read_csv(train_labels)
+
+df_train = train_df[:5000]
+
+#df_test = train_df.iloc[12000:25361]
+
+#print(df_test.describe())
 
 print(train_df.shape)
 
@@ -54,16 +63,31 @@ def label_prep(y):
     y = onehot_encoded
     return y
 
-X = image_prep(train_df, train_df.shape[0], "train")
+X = image_prep(df_train, df_train.shape[0], "train")
 
 X /= 255
+
+#X_test = image_prep(df_test, df_test.shape[0], "train")
+
+#X_test /= 255
         
-y = label_prep(train_df['Id'])
+y = label_prep(df_train['Id'])
 
 y_labels = np.argmax(y,axis=1)
 
-print(y.shape)
+print(y[:5])
 
+print(y_labels[:5])
+
+#y_test = label_prep(df_test['Id'])
+
+#y_labels_test = np.argmax(y_test,axis=1)
+
+#print(y_test)
+
+#print(y_labels)
+
+'''
 def plot_images(images, cls_true):
     assert len(images) == len(cls_true)     
     fig, axes = plt.subplots(3, 3)
@@ -77,6 +101,48 @@ def plot_images(images, cls_true):
     plt.show()
 
 plot_images(X[:9],y_labels[:9])
+'''
+
+base_model = VGG16(weights='imagenet',input_shape=(224,224,3), include_top=False)
+x = base_model.output
+x = Flatten()(x)
+x = Dense(512,activation='relu')(x)
+preds = Dense(y.shape[1], activation='softmax', name='sm')(x)
+model = Model(inputs=base_model.input,outputs=preds)
+
+model.compile(loss='categorical_crossentropy', optimizer="adam", metrics=['accuracy'])
+model.summary() 
+
+history = model.fit(X, y,validation_split=0.3, epochs=100, batch_size=256, verbose=1)
+
+'''
+model.add(Dense(y.shape[1], activation='softmax', name='sm'))
+model.compile(loss='categorical_crossentropy', optimizer="adam", metrics=['accuracy'])
+model.summary()
+
+history = model.fit(X, y,validation_split=0.3, epochs=100, batch_size=100, verbose=1,callbacks=[early_stopping_monitor])
+
+#features_train = model.predict(X)
+
+#print(features_train)
+'''
+
+'''
+model= Sequential()
+
+model.add(Flatten())
+
+model.add(Dense(500, activation="relu", name='rl'))
+model.add(Dropout(0.8))
+
+model.add(Dense(y.shape[1], activation='softmax', name='sm'))
+
+model.compile(loss='categorical_crossentropy', optimizer="adam", metrics=['accuracy'])
+model.summary()
+
+history = model.fit(x, y,validation_split=0.3, epochs=100, batch_size=100, verbose=1,callbacks=[early_stopping_monitor])
+'''
+
 
 model = Sequential()
 
@@ -137,4 +203,12 @@ model.add(Dense(y.shape[1], activation='softmax', name='sm'))
 model.compile(loss='categorical_crossentropy', optimizer="adam", metrics=['accuracy'])
 model.summary()
 
-history = model.fit(X,y,validation_split=0.3, epochs=1000, batch_size=256, verbose=1)
+history = model.fit(X,y,validation_split=0.3, epochs=2, batch_size=7, verbose=1)
+
+y_pred = model.predict_classes(X_test)
+
+y_pred_proba = model.predict_proba(X_test)
+
+print(y_pred)
+
+print(y_pred_proba)
